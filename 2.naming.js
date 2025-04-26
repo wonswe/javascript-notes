cca9722daa16eb209d37c17a6f84089c5b37ca63
woncodes <64626054+woncodes@users.noreply.github.com>
2025-04-26 11:53:08 -0400

    start 2.naming.js

---
+++ 2.naming.js
@@ -0,0 +1,20 @@
+/* 
+주석 
+라틴문자 (0-9, a-z, A-Z), _
+추천: camelCase (likeThis)
+한국어 x
+예약어 x
+숫자로 시작 x
+특수문자 x (_, $ 두가지는 제외)
+이모지 x
+*/
+
+let apple;
+let redApple;
+
+
+// 나쁜예제 
+let number = 20;
+
+// 좋은예제
+let myAge = 20;
