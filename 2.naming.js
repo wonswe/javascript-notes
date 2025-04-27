0ecaaa84f0116f1ba7294ae6f56a682aaca879f6
woncodes <64626054+woncodes@users.noreply.github.com>
2025-04-26 23:54:24 -0400

    complete 2.naming.js

--- 2.naming.js
+++ 2.naming.js
@@ -1,13 +1,13 @@
-/* 
-주석 
-라틴문자 (0-9, a-z, A-Z), _
-추천: camelCase (likeThis)
-한국어 x
-예약어 x
-숫자로 시작 x
-특수문자 x (_, $ 두가지는 제외)
-이모지 x
-*/
+// 주석 
+// 라틴문자 (0-9, a-z, A-Z), _
+// 추천: camelCase (likeThis)
+// 한국어 x
+// 예약어 x
+// 숫자로 시작 x
+// 특수문자 x (_, $ 두가지는 제외)
+// 이모지 x
+// 여러개의 변수를 1, 2, 3 숫자로 구분하지 않고 최대한 의미있게, 구체적인 이름으로 작성
+
 
 let apple;
 let redApple;
@@ -18,3 +18,15 @@ let number = 20;
 
 // 좋은예제
 let myAge = 20;
+
+// 나쁜예제
+let audio1;
+let audio2;
+
+// 좋은예제
+let backgroundAudio;
+let windAudio;
+
+// Tip
+let audioBackground;
+let audioWind;
