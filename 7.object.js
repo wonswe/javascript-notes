f608c3ee7d41a0d264798c5631edc0239300f7ec
woncodes <64626054+woncodes@users.noreply.github.com>
2025-05-01 22:37:01 -0400

    complete 7.object.js

---
+++ 7.object.js
@@ -0,0 +1,31 @@
+// 객체 object: 복합 데이터 {key:value}
+// 연관된 데이터를 한 덩어리로 그룹 지을 수 있다
+// heap 에 보관
+
+let name = 'apple';
+let color = 'red';
+let display = '🍎';
+let orangeName = 'orange';
+
+let apple = {
+    name: 'apple',
+    color: 'red',
+    display: '🍎',
+}
+
+console.log(apple);
+console.log(apple.name);
+console.log(apple.color);
+console.log(apple.display);
+
+
+let orange = {
+    name: '오렌지',
+    color: '주황색',
+    display: '🍊',
+}
+
+console.log(orange);
+console.log(orange.name);
+console.log(orange.color);
+console.log(orange.display);
