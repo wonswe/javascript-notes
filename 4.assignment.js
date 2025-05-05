d12bf459c2e32b524eefc56e49100b733a12d24a
woncodes <64626054+woncodes@users.noreply.github.com>
2025-05-05 15:25:24 -0400

    complete 4.assignment.js

---
+++ 4.assignment.js
@@ -0,0 +1,20 @@
+// 할당연산자 (assignment operators)
+let a = 1;
+a = a + 2;
+console.log(a);
+
+a += 2; // 5
+console.log(a); // a = a + 2; 의 축약
+
+a -= 2; // 3
+console.log(a);
+
+a *= 2; // 6
+console.log(a);
+
+a /= 2; // 3
+console.log(a);
+a %= 2; // 1
+console.log(a);
+a **= 2; // 1
+console.log(a);
