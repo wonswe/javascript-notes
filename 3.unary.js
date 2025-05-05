a827d9dd185a3356cccd66f43adb3bf76b1c290d
woncodes <64626054+woncodes@users.noreply.github.com>
2025-05-05 15:20:40 -0400

    complete 3.unary.js

--- 3.unary.js
+++ 3.unary.js
@@ -2,3 +2,34 @@
 // + (양)
 // - (음)
 // ! (부정)
+
+let a = 5;
+a = -a; // -1 * 5
+console.log(a); //-5
+a = -a; // -1 * -5
+console.log(a); // 5
+
+a = +a;
+console.log(a); // 5
+
+a = -a; // -5
+a = +a; // +(-5)
+console.log(a); // -5
+
+let boolean = true;
+console.log(boolean); //true
+console.log(!boolean); // false
+console.log(!!boolean); // true
+
+// + 숫자가 아닌 타입들을 숫자로 변환하면 어떤 값이 나오는지 확인할 수 있음
+console.clear();
+console.log(+false); // 0
+console.log(+null); // 0
+console.log(+''); // 0
+
+console.log(+true); // 1
+console.log(+'text'); // NaN
+console.log(+undefined); // NaN
+
+console.log(!1); // false - ! 부정연산자
+console.log(!!1); // true - !! 값을 boolean 타입으로 변환함
