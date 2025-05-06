dcadfdd8076bda551151570e9198f37a402ca49f
woncodes <64626054+woncodes@users.noreply.github.com>
2025-05-05 21:05:54 -0400

    complete 6.relational.js

---
+++ 6.relational.js
@@ -0,0 +1,14 @@
+// 대소 관계 비교 연산자 (Relational Operator)
+// > 크다
+// < 작다
+// >= 크거나 같다
+// <= 작거나 같다
+
+console.log(2 > 3); // false
+console.log(2 < 3); // true
+console.log(3 < 2); // false
+console.log(3 > 2); // true
+console.log(3 <= 2); // false
+console.log(3 <= 3); // true
+console.log(3 >= 3); // true
+console.log(3 >= 2); // true
