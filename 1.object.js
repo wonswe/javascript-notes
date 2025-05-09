90f4c04bc5652b34246750391191d54bb96ccdc2
woncodes <64626054+woncodes@users.noreply.github.com>
2025-05-09 14:05:10 -0400

    complete 1.object.js

--- 1.object.js
+++ 1.object.js
@@ -14,4 +14,37 @@ let apple = {
 // 객체는 heap이라는 메모리 공간에 존재
 // apple이라는 변수 자체는 실제 객체가 만들어진 메모리 주소를 가리킨다
 
-console.log(`apple's name is ${apple.name}`);
+// console.log(`apple's name is ${apple.name}`);
+
+// Object Literal { key: value }
+// new Ojbect()
+// Object.create();
+// key - 문자, 숫자, 문자열, 심볼
+// value - 원시값, 객체 (함수)
+let orange = {
+  name: 'orange',
+  'hello-bye': '✋',
+  0: 1,
+  ['hello-bye1']: '✋',
+};
+
+// 가능하면 camelCase를 이용해서 helloBye 의 형식으로 만드는게 좋다
+
+// 속성, 데이터에 접근하기 위해서는
+console.log(orange.name); // 마침표 표기법 dot notation
+
+console.log(orange['hello-bye1']);
+// 대괄호 표기법 bracket notation
+
+// 모든 오브젝트의 키는 이렇게 접근 가능
+orange['name'];
+
+// 속성 추가
+orange.emoji = '🍊';
+console.log(orange.emoji);
+console.log(orange['emoji']);
+console.log(orange);
+
+// 속성 삭제
+delete orange.emoji;
+console.log(orange);
