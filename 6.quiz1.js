50960aee29c45377daf95a96cefaa449f787e7ef
woncodes <64626054+woncodes@users.noreply.github.com>
2025-05-11 10:29:05 -0400

    complete quiz1.js

--- 6.quiz1.js
+++ 6.quiz1.js
@@ -1,2 +1,30 @@
-// 카운터 만들기
+// Counter 클래스 만들기
 // 0 이상의 값으로 초기화 한 뒤, 하나씩 숫자를 증가할 수 있는 카운터를 만들기
+class Counter {
+  #value;
+  constructor(startValue) {
+    if (isNaN(startValue) || startValue < 0) {
+      this.#value = 0;
+    } else {
+      this.#value = startValue;
+    }
+  }
+
+  get value() {
+    // 현재 값을 읽는다
+    return this.#value;
+  }
+
+  increment = () => {
+    // 값을 증가
+    this.#value++;
+  };
+}
+
+const counter = new Counter(0);
+counter.increment();
+console.log(counter.value); // 1
+counter.increment();
+console.log(counter.value); // 2
+counter.increment();
+console.log(counter.value); // 3
