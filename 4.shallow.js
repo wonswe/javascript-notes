5b80a1644b9f791ab98f237547a466e3bcd5e59d
woncodes <64626054+woncodes@users.noreply.github.com>
2025-05-18 07:44:09 -0400

    continue 5.quiz.js

--- 4.shallow.js
+++ 4.shallow.js
@@ -2,9 +2,9 @@
 // 자바스크립트에서 복사할때는 항상 얕은 복사가 이루어짐!
 // Array.from, concat, slice, spread(...), Object.assign
 
-function(obj) {
-    // 함수 내부에서 오브젝트를 수정하는 것은 절대 좋지 않다. ❌
-}
+// function(obj) {
+//     함수 내부에서 오브젝트를 수정하는 것은 절대 좋지 않다. ❌
+// }
 
 const pizza = { name: '🍕', price: 2, owner: { name: 'Juwon' } };
 const ramen = { name: '🍜', price: 3 };
