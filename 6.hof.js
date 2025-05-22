ef1bc8ad32001fb01508e60feb08aa7ea8ca294c
woncodes <64626054+woncodes@users.noreply.github.com>
2025-05-21 23:45:59 -0400

    complete 6.hof.js

--- 6.hof.js
+++ 6.hof.js
@@ -54,3 +54,56 @@ result = nums.map((item) => {
 });
 
 console.log(result); // [ 1, 4, 3, 8, 5 ]
+
+// Flatmap
+result = nums.map((item) => [1, 2]);
+console.log(result);
+// [ [ 1, 2 ], [ 1, 2 ], [ 1, 2 ], [ 1, 2 ], [ 1, 2 ] ]
+
+result = nums.flatMap((item) => [1, 2]);
+console.log(result);
+
+// [
+//     1, 2, 1, 2, 1,
+//     2, 1, 2, 1, 2
+//   ]
+
+result = ['dream', 'coding'].map((text) => text.split(''));
+console.log(result);
+// [ [ 'd', 'r', 'e', 'a', 'm' ], [ 'c', 'o', 'd', 'i', 'n', 'g' ] ]
+
+result = ['dream', 'coding'].flatMap((text) => text.split(''));
+console.log(result);
+// [
+//     'd', 'r', 'e', 'a',
+//     'm', 'c', 'o', 'd',
+//     'i', 'n', 'g'
+//   ]
+
+// Sort 배열의 아이템들을 정렬
+// 문자열 형태의 오름차순으로 요소를 정렬하고, 기존의 배열을 변경
+const texts = ['hi', 'abc'];
+texts.sort();
+console.log(texts);
+// [ 'abc', 'hi' ]
+
+const numbers = [0, 5, 4, 2, 1, 10];
+numbers.sort();
+console.log(numbers);
+// [ 0, 1, 10, 2, 4, 5 ]
+
+// < 0 , a가 앞으로 정렬, 오름차순
+// > 0, b가 앞으로 정렬, 내림차순
+numbers.sort((a, b) => a - b);
+console.log(numbers);
+// [ 0, 1, 2, 4, 5, 10 ]
+
+// reduce 배열의 요소들을 접어서 접어서 값을 하나로
+result = [1, 2, 3, 4, 5].reduce((sum, value) => {
+  sum += value;
+  return sum;
+}, 0);
+console.log(result); // 15
+
+result = [1, 2, 3, 4, 5].reduce((sum, value) => (sum += value), 0);
+console.log(result); // 15
