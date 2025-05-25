96a1748b21b05a50e6b328fc58fe49a05b6419c4
woncodes <64626054+woncodes@users.noreply.github.com>
2025-05-24 23:30:43 -0400

    complete 2.iterable-quiz.js

--- 2.iterable-quiz.js
+++ 2.iterable-quiz.js
@@ -2,12 +2,28 @@
 // 0부터 10이하까지 숫자의 2배를 순회하는 이터레이터(반복자) 만들기!
 // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 // 0, 2, 4, 6, 8, 10, 12, 14, 16, 18 - 두배값의 순회
-const iterator = array.values();
+
+// const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const iterator = array.values();
+// while (true) {
+//   const item = iterator.next();
+//   if (item.done) break;
+//   console.log(item.value * 2);
+// }
 
 const multiple = {
-  [Symbol.iterator]() {},
+  [Symbol.iterator]() {
+    const max = 10;
+    let num = 0;
+    return {
+      next() {
+        return { value: num++ * 2, done: num > max };
+      },
+    };
+  },
 };
 
 for (const num of multiple) {
-  console.log(num); // 0, 2, 4, 6, 8, 10, 12, 14, 16, 18
+  console.log(num);
 }
