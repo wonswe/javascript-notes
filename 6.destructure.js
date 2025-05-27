a2a4741e3ef727440260444d805f838975dc34c7
woncodes <64626054+woncodes@users.noreply.github.com>
2025-05-26 22:30:22 -0400

    complete 6.destructure.js

--- 6.destructure.js
+++ 6.destructure.js
@@ -12,3 +12,29 @@ const [x, y, z = 0] = point;
 console.log(x); // 1
 console.log(y); // 2
 console.log(z); // 8
+
+function createEmoji() {
+  return ['apple', '🍎'];
+}
+
+const [title, emoji] = createEmoji(); // 배열의 아이템을 각각 구조분해하여 할당
+console.log(title);
+console.log(emoji);
+
+const juwon = { name: 'juwon', age: 20, job: 'software engineer' };
+// function display(person) {
+//   console.log('이름:', person.name);
+//   console.log('나이:', person.age);
+//   console.log('직업:', person.job);
+// }
+
+function display({ name, age, job }) {
+  console.log('이름:', name);
+  console.log('나이:', age);
+  console.log('직업:', job);
+}
+
+display(juwon);
+
+const { name, age, job: occupation, pet = '고양이' } = juwon;
+console.log(name, age, occupation, pet);
