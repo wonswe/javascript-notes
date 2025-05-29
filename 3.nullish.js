79634158a27b7729c60dabc8a1b241074db57c5b
woncodes <64626054+woncodes@users.noreply.github.com>
2025-05-29 16:14:16 -0400

    continue 3.nullish.js

---
+++ 3.nullish.js
@@ -0,0 +1,6 @@
+// Nullish Coalescing Operator
+// ES11 (ECMAScript 2020)
+// ?? null, undefinedx
+// || falsy한 경우 설정(할당)
+let num = 0; // 0은 falsy한 값으로 간주되어서 기본값으로 아래 -1로 설정한다.
+console.log(num || '-1'); // -1
