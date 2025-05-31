d519db279d047fc11afacbfd7657dddcdf3c4f1f
woncodes <64626054+woncodes@users.noreply.github.com>
2025-05-31 17:57:45 -0400

    complete 3.nullish.js

--- 11.more-operators/3.nullish.js
+++ 11.more-operators/3.nullish.js
@@ -1,6 +1,22 @@
 // Nullish Coalescing Operator
 // ES11 (ECMAScript 2020)
-// ?? null, undefinedx
+// ?? null, undefined
 // || falsy한 경우 설정(할당)
-let num = 0; // 0은 falsy한 값으로 간주되어서 기본값으로 아래 -1로 설정한다.
+let num = null;
+
+// 0은 falsy한 값으로 간주되어서 기본값이 아래 -1로 설정된다.
 console.log(num || '-1'); // -1
+
+console.log((null || undefined) ?? 'foo'); // foo
+
+const nullValue = null;
+const emptyText = ''; // falsy
+const someNumber = 42;
+
+const valA = nullValue ?? 'default for A';
+const valB = emptyText ?? 'default for B';
+const valC = someNumber ?? 0;
+
+console.log(valA); // "default for A"
+console.log(valB); // "" (as the empty string is not null or undefined)
+console.log(valC); // 42
