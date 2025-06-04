1ff7fa132ef4dd0578eb88eb5993d98756d8dfea
woncodes <64626054+woncodes@users.noreply.github.com>
2025-06-03 22:50:54 -0400

    continue 1.callstack.js

--- 1.callstack.js
+++ 1.callstack.js
@@ -2,3 +2,17 @@
 // callback
 // promise
 // async/await
+function a() {
+  return 1;
+}
+
+function b() {
+  return a() + 1;
+}
+
+function c() {
+  return b() + 1;
+}
+
+const result = c();
+console.log(result); // 3
