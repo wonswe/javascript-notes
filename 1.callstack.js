1593504ca73390f9ae7fea8c2c15d8f9f0c755fd
woncodes <64626054+woncodes@users.noreply.github.com>
2025-06-05 23:33:57 -0400

    add notes to 1.callstack.js

--- 1.callstack.js
+++ 1.callstack.js
@@ -1,7 +1,8 @@
-// 비동기 Async
-// callback
-// promise
-// async/await
+// 소스코드에서 동적으로 객체를 생성할 때 Memory Heap에 생성이 된다.
+
+// 하나의 싱글 컨텍스트 스택 - 한번에 하나의 일만 처리, 싱글 쓰레드
+// 자바스크립트는 기본적으로 동기적으로 진행
+
 function a() {
   for (let i = 0; i < 1000000000; i++);
   return 1;
