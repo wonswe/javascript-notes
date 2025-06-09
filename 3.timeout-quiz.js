c86c192fe657c5b65b4885ce4ea62c2fc9c887e8
woncodes <64626054+woncodes@users.noreply.github.com>
2025-06-08 23:47:01 -0400

    continue 3.timeout-quiz.js

--- 3.timeout-quiz.js
+++ 3.timeout-quiz.js
@@ -1,11 +1,11 @@
 // 주어진 seconds(초)가 지나면 callback 함수를 호출함
 // 단, seconds가 0보다 작으면 에러를 던지자!
 
-function runInDelay(callback, seconds) {
-    try {
-        
-    }
-}
+// function runInDelay(callback, seconds) {
+//     try {
+
+//     }
+// }
 
 function execute() {
   console.log('1');
