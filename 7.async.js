27ce992a6fb7f44eff8b66a51b1f37674bae1c8c
woncodes <64626054+woncodes@users.noreply.github.com>
2025-06-11 23:33:51 -0400

    start 7.async.js

---
+++ 7.async.js
@@ -0,0 +1,21 @@
+function getBanana() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('🍌');
+    }, 1000);
+  });
+}
+
+function getApple() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('🍎');
+    }, 3000);
+  });
+}
+
+function getOrange() {
+  return Promise.reject(new Error('no orange'));
+}
+
+// 바나나와 사과를 같이 가지고 오기
