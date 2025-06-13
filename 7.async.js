830a7508d47b69b87dd79dd0dc13abcade311d6c
woncodes <64626054+woncodes@users.noreply.github.com>
2025-06-12 22:35:30 -0400

    complete 7.async.js

--- 7.async.js
+++ 7.async.js
@@ -19,3 +19,19 @@ function getOrange() {
 }
 
 // 바나나와 사과를 같이 가지고 오기
+async function fetchFruits() {
+  const banana = await getBanana();
+  const apple = await getApple();
+  return [banana, apple];
+
+  // ⬆️ 아래와 같은 코드지만 async와 await을 사용
+  return getBanana() //
+    .then((banana) =>
+      getApple() //
+        .then((apple) => [banana, apple])
+    )
+    .then(console.log);
+}
+
+fetchFruits() //
+  .then((fruits) => console.log); // [ '🍌', '🍎' ]
