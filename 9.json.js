c976f143380d8e18caef26183a8c167b17b52eea
woncodes <64626054+woncodes@users.noreply.github.com>
2025-06-13 23:59:45 -0400

    complete 9.json.js

--- 9.json.js
+++ 9.json.js
@@ -3,3 +3,21 @@
 // 오브젝트 형태의 텍스트 포맷
 // stringify(object): JSON
 // parse)JSON: object
+
+const juwon = {
+  name: 'juwon',
+  age: 20,
+  eat: () => {
+    console.log('eat');
+  },
+};
+
+console.log(juwon);
+// { name: 'juwon', age: 20, eat: [Function: eat] }
+
+const json = JSON.stringify(juwon);
+console.log(json);
+// {"name":"juwon","age":20}
+
+const obj = JSON.parse(json);
+console.log(obj);
