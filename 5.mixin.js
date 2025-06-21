a85b2f775dcb9dae9e41c2ca7d5d05c8492ee3c6
woncodes <64626054+woncodes@users.noreply.github.com>
2025-06-20 23:02:35 -0400

    complete 5.mixin.js

--- 5.mixin.js
+++ 5.mixin.js
@@ -2,5 +2,36 @@
 // 하지만! 여러개의 함수들을 상속하고 싶을 때 사용하는 Mixin!
 
 const play = {
-  play: function () {},
+  play: function () {
+    console.log(`${this.name} 놀아요!`);
+  },
 };
+
+const sleep = {
+  sleep: function () {
+    console.log(`${this.name} 자요!`);
+  },
+};
+
+function Dog(name) {
+  this.name = name;
+}
+
+Object.assign(Dog.prototype, play, sleep);
+const dog = new Dog('멍멍');
+console.log(dog);
+dog.play(); // 멍멍 놀아요!
+dog.sleep(); // 멍멍 자요!
+
+class Animal {}
+class Tiger extends Animal {
+  constructor(name) {
+    super();
+    this.name = name;
+  }
+}
+
+Object.assign(Tiger.prototype, play, sleep);
+const tiger = new Tiger('어흥');
+tiger.play(); // 어흥 놀아요!
+tiger.sleep(); // 어흥 자요!
