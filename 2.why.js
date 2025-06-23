57544aadbe5bee84dde7cee1e4f6925935c2dd1a
woncodes <64626054+woncodes@users.noreply.github.com>
2025-06-22 22:50:20 -0400

    complete 2.why.js

---
+++ 2.why.js
@@ -0,0 +1,33 @@
+// 내부 정보를 은닉하고, 공개 함수(public, 외부)를 통한 데이터 조작을 위해
+// 캡슐화와 정보 은닉
+// 클래스 private 필드 또는 메소드를 사용하는 효과와 동일!
+
+function makeCounter() {
+  let count = 0;
+  function increase() {
+    count++;
+    console.log(count);
+  }
+  return increase;
+}
+
+const increase = makeCounter();
+increase();
+increase();
+increase();
+
+// 이제는 위처럼 클로져를 사용하지 않고 클래스를 이용해서
+// private 필드를 사용한다!
+
+class Counter {
+  #count = 0;
+  increase() {
+    this.#count++;
+    console.log(this.#count);
+  }
+}
+
+const counter = new Counter();
+counter.increase();
+counter.increase();
+counter.increase();
