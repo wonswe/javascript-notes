cd4a1b9879212d25312fa6676927c4f9fa749af3
woncodes <64626054+woncodes@users.noreply.github.com>
2025-06-23 20:37:44 -0400

    complete 18.babel

---
+++ 18.babel/build/index.js
@@ -0,0 +1,73 @@
+'use strict';
+
+require('core-js/modules/es.symbol.js');
+require('core-js/modules/es.symbol.description.js');
+require('core-js/modules/es.symbol.iterator.js');
+require('core-js/modules/es.symbol.to-primitive.js');
+require('core-js/modules/es.array.iterator.js');
+require('core-js/modules/es.date.to-primitive.js');
+require('core-js/modules/es.number.constructor.js');
+require('core-js/modules/es.object.define-property.js');
+require('core-js/modules/es.string.iterator.js');
+require('core-js/modules/web.dom-collections.iterator.js');
+function _typeof(o) {
+  '@babel/helpers - typeof';
+  return (
+    (_typeof =
+      'function' == typeof Symbol && 'symbol' == typeof Symbol.iterator
+        ? function (o) {
+            return typeof o;
+          }
+        : function (o) {
+            return o &&
+              'function' == typeof Symbol &&
+              o.constructor === Symbol &&
+              o !== Symbol.prototype
+              ? 'symbol'
+              : typeof o;
+          }),
+    _typeof(o)
+  );
+}
+require('core-js/modules/es.object.to-string.js');
+require('core-js/modules/es.promise.js');
+function _defineProperties(e, r) {
+  for (var t = 0; t < r.length; t++) {
+    var o = r[t];
+    (o.enumerable = o.enumerable || !1),
+      (o.configurable = !0),
+      'value' in o && (o.writable = !0),
+      Object.defineProperty(e, _toPropertyKey(o.key), o);
+  }
+}
+function _createClass(e, r, t) {
+  return (
+    r && _defineProperties(e.prototype, r),
+    t && _defineProperties(e, t),
+    Object.defineProperty(e, 'prototype', { writable: !1 }),
+    e
+  );
+}
+function _toPropertyKey(t) {
+  var i = _toPrimitive(t, 'string');
+  return 'symbol' == _typeof(i) ? i : i + '';
+}
+function _toPrimitive(t, r) {
+  if ('object' != _typeof(t) || !t) return t;
+  var e = t[Symbol.toPrimitive];
+  if (void 0 !== e) {
+    var i = e.call(t, r || 'default');
+    if ('object' != _typeof(i)) return i;
+    throw new TypeError('@@toPrimitive must return a primitive value.');
+  }
+  return ('string' === r ? String : Number)(t);
+}
+function _classCallCheck(a, n) {
+  if (!(a instanceof n))
+    throw new TypeError('Cannot call a class as a function');
+}
+var Cat = /*#__PURE__*/ _createClass(function Cat() {
+  _classCallCheck(this, Cat);
+});
+var hello = function hello() {};
+var yes = Promise.resolve(1);
